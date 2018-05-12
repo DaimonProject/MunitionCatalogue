d476df2db90ab101f641949f3dfc099f63868328
Philipp Kraus <philipp.kraus@flashpixx.de>
2018-05-12 15:13:49 +0200

    country structure

--- country.php
+++ country.php
@@ -4,7 +4,7 @@
     $la_result = array();
 
     $lo_con = dbconnect();
-    $lo_query = pg_query( $lo_con, "select id, iso639_3, name from daimon.country order by iso639_3" );
+    $lo_query = pg_query( $lo_con, "select iso639_1, name from daimon.country order by lower(name)" );
     while( $lo_data = pg_fetch_object( $lo_query ) )
         array_push( $la_result, $lo_data );
 
--- index.php
+++ index.php
@@ -7,20 +7,36 @@
             jQuery("#tree").jstree({
                 "core" : {
                     "data" : {
-                        "url" : "'.  linkprefix( '/tree.php?lazy' ) .'",
+                        "url" : "'.linkprefix( '/tree.php?lazy' ).'",
                         "data" : function (n) { return { "id" : n.id }; }
                     }
                 }
             });
-        
+
+            jQuery.ajax("'.linkprefix( '/country.php' ).'")
+                  .done( function(n) {
+                    n.forEach( function(i) {
+                        jQuery("#origin").append( 
+                            jQuery("<option>").val( i.iso639_1 )
+                                              .text( i.name + " (" + i.iso639_1 + ")" )
+                        );
+                    });
+                  });
     });</script>';
 
 
-
+    echo '<div class="container">';
     echo '<div class="row">';
     echo '<div id="tree" class="col-sm-4"></div>';
-    echo '<div class="col-sm-6">Eingabeform</div>';
+    echo '<div class="col-sm-6">';
+
+    echo '<form>';
+    echo '<div class="form-group"><label for="origin">Herstellung in</label><select class="form-control" id="origin"></select></div>';
+    echo '</form>';
+
+    echo '</div>';
     echo "</div>";
+    echo '</div>';
 
 
     htmlfooter();
